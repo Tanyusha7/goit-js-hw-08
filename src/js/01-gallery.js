6b5e3ac51f1ce083c67e4e24d7de7ac6aed18919
Tanyusha7 <swanonlake@gmail.com>
2023-05-02 23:09:33 +0200

    add siplelightbox, update index.js, 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
+import 'simplelightbox/dist/simple-lightbox.esm';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryPictures = document.querySelector('.gallery');
+
+const newGallery = createPicturesMarkup(galleryItems);
+
+galleryPictures.insertAdjacentHTML('beforeend', newGallery);
+
+galleryPictures.addEventListener('click', onGalleryPictures);
+
+console.log(galleryPictures);
+
+function createPicturesMarkup(picture) {
+  return picture
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}"  >
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`;
+    })
+    .join(' ');
+}
+function onGalleryPictures(evt) {
+  evt.preventDefault();
+
+  let lightbox = $('.gallery__link').simpleLightbox({
+    captionsData: 'alt',
+    captionDelay: '250',
+  });
+
+  console.log(lightbox);
+}
 
 console.log(galleryItems);
